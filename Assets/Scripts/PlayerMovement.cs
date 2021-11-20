150542ea80d6b527d04bb82f477863bc84dcf080
Chowdog <yangerplato3465@gmail.com>
2021-11-21 00:26:07 +0800

    light

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed;
+    public Rigidbody2D body;
+    private Vector2 moveInput;
+    void Start() {
+    }
+
+    void Update() {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        body.velocity = moveInput * moveSpeed;
+    }
+
+    
+}
