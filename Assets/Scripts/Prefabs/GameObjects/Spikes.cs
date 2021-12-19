833e876d828896670b923b4d2b2e022c4fd23194
Chowdog <>
2021-12-19 17:01:18 +0800

    moving spike

---
+++ Assets/Scripts/Prefabs/GameObjects/Spikes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+    
+    public bool isMoving = false;
+    public float smoothTime = 0.15f;
+    public Vector3 startPoint;
+    public Vector3 endPoint;
+    private Vector3 velocity = Vector3.zero;
+    private bool toEnd = true;
+
+    private void Update() {
+        if(transform.position == startPoint) toEnd = true;
+        if(transform.position == endPoint) toEnd = false;
+        
+        if(toEnd) transform.position = Vector3.SmoothDamp(transform.position, endPoint, ref velocity, smoothTime);
+        else transform.position = Vector3.SmoothDamp(transform.position, startPoint, ref velocity, smoothTime);
+    }
+
+}
